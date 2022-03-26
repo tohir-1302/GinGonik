10794bffc376113fea1cbb044c7a5ef8cb8b3d1e
tony-1302 <tohirbek.otaqulov1302@mail.ru>
2022-03-26 09:02:47 +0500

    chala. xatolik bergani uchun boshqa kursga utildi

--- server.go
+++ server.go
@@ -1,14 +1,24 @@
 package main
 
-import "github.com/gin-gonic/gin"
-
+import (
+	"gilab.com/pragmaticreviews/golang-gin-poc/controller"
+	"gilab.com/pragmaticreviews/golang-gin-poc/service"
+	"github.com/gin-gonic/gin"
+)
+var(
+	videoService service.VideoService = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
 func main()  {
 	server := gin.Default()
 
-	server.GET("/test", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message" : "Salomm",
-		})
+	server.GET("/posts", func(ctx *gin.Context){
+		ctx.JSON(200,videoController.FindAll())
+	})
+
+
+	server.POST("/posts", func(ctx *gin.Context){
+		ctx.JSON(200,videoController.Save(ctx))
 	})
 
 	server.Run(":8080")
